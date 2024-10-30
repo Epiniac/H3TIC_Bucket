4f6477f2b664ec9e919d9b969a593887d808ca3e
lsun <lsun.github@gmail.com>
2024-10-30 11:04:51 +0100

    init project auth

--- file-server/app.js
+++ file-server/app.js
@@ -1,11 +1,13 @@
 import express from "express";
-import router from "./route/fileRoutes.js";
+import fileRoutes from "./route/fileRoutes.js";
+import authRoutes from "./route/authRoutes.js"
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use("files", router);
+app.use("files", fileRoutes);
+app.use("auth", authRoutes);
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
---
+++ file-server/controllers/authController.js
@@ -0,0 +1,9 @@
+import connection from "../models/db.js";
+
+export const register = async (req, res) => {
+
+}
+
+export const login = async (req, res) => {
+
+}
